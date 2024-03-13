fa1670639fbacb21a528f421472fa2ba28675995
Charles <charlethical1@gmail.com>
2024-03-13 15:14:34 +0200

    Converted logout function view into class view

--- NoteBlogRestApi/user_app/views.py
+++ NoteBlogRestApi/user_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,16 +10,24 @@ from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
-@api_view(["POST",])
-@authentication_classes([TokenAuthentication])
-def logout_user(request):
-    if request.user.is_authenticated:
-        # Only try to delete the token if the user is authenticated
+# @api_view(["POST",])
+# @authentication_classes([TokenAuthentication])
+# def logout_user(request):
+#     if request.user.is_authenticated:
+#         # Only try to delete the token if the user is authenticated
+#         request.user.auth_token.delete()
+#         return Response({'success': 'Successfully logged out.'}, status=200)
+#     else:
+#         # Handle the case where the user is not authenticated
+#         return Response({'error': 'You are not logged in.'}, status=400)
+
+class LogoutUserView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
         request.user.auth_token.delete()
-        return Response({'success': 'Successfully logged out.'}, status=200)
-    else:
-        # Handle the case where the user is not authenticated
-        return Response({'error': 'You are not logged in.'}, status=400)
+        return Response({'Success': 'Successfully logged out.'}, status=200)
 
 class UserRegisterView(APIView):
     permission_classes = [AllowAny]
